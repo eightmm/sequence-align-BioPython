6034e7d0e217b32b7f296d81cff58b6f87f4b417
eightmm <sjm0775@naver.com>
2024-09-05 16:15:44 +0900

    change example

--- example/run.sh
+++ example/run.sh
@@ -1,1 +1,5 @@
-python ../sequence_align.py --temp 3BEX.pdb --query seq.txt --out output.tsv
+
+python ../sequence_align.py --temp seq.txt --query seq.txt --out out0.tsv
+python ../sequence_align.py --temp 3BEX.pdb --query seq.txt --out out1.tsv
+python ../sequence_align.py --temp seq.txt --query 3BEX.pdb --out out2.tsv
+python ../sequence_align.py --temp 3BEX.pdb --query 3BEX.pdb --out out3.tsv
